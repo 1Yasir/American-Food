4717e20ac12ead36aa5775382322b56c73f4784a
Yasir Mehmood <azmyasir250@gmail.com>
2023-08-04 10:54:50 -0700

    add btn scroll up

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function activeLink(eve){
     // only current link add active class 
     eve.currentTarget.classList.add("active");
 
-}
+};
 
 // navBardisplay display block and none function  
 
@@ -16,4 +16,18 @@ function navBardisplay(){
     let navBar = document.querySelector(".navbar");
     navBar.classList.toggle("block");
    
-}
+};
+
+// arrow btn script start
+
+window.addEventListener("scroll" ,function(){
+    let btn = this.document.querySelector(".up");
+
+    if(this.scrollY > 10){
+        btn.style.display = "inline-block";
+    }else{
+        btn.style.display = "none";
+
+    }
+});
+// arrow btn script end
